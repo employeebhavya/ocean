fcefd61b0aa03e4b08c1fd5cc138cbe06e51761d
Ritu Raj <145446035+riturajroman@users.noreply.github.com>
2025-03-11 18:56:50 +0530

    updated logos and team

--- src/componentsAbout/Team.js
+++ src/componentsAbout/Team.js
@@ -1,14 +1,8 @@
 "use client";
 
 /* eslint-disable @next/next/no-img-element */
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import styles from "./Team.module.css";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-if (typeof window !== "undefined") {
-  gsap.registerPlugin(ScrollTrigger);
-}
 
 const Members = [
   {
@@ -54,47 +48,6 @@ function Team() {
   const decRef = useRef(null);
   const memberRefs = useRef([]);
 
-  useEffect(() => {
-    const context = gsap.context(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          trigger: sectionRef.current,
-          start: "top 80%",
-          end: "bottom 100%",
-          scrub: 1,
-        },
-      });
-
-      // Animate heading
-      tl.from(headingRef.current, {
-        opacity: 0,
-        y: 50,
-        duration: 1,
-      });
-
-      tl.from(decRef.current, {
-        opacity: 0,
-        y: 50,
-        duration: 1,
-      });
-      // Staggered animation for team member boxes
-      tl.from(
-        memberRefs.current,
-        {
-          opacity: 0,
-          y: 50,
-          duration: 0.5,
-          stagger: 0.2,
-        },
-        "-=0.5"
-      ); // Start member animation just after heading
-    }, sectionRef);
-
-    return () => {
-      context.revert();
-    };
-  }, []);
-
   return (
     <section ref={sectionRef} className={styles.team}>
       <div className="container">
@@ -116,7 +69,7 @@ function Team() {
                 className={styles.teamBox}
                 ref={(el) => (memberRefs.current[index] = el)}
               >
-                <img src={member.image} alt="image" />
+                <img src={member.image} alt={member.title} />
                 <div className={styles.contentTeamBox}>
                   <h3 className="h4">{member.title}</h3>
                   <p className="description">{member.description}</p>
