f2458f9560a9d7a841047a02aa5a7969cfab9ab1
Ritu Raj <145446035+riturajroman@users.noreply.github.com>
2025-04-29 12:29:55 +0530

    added meta code gtag

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Footer from "@/components/Footer";
 import Menu from "@/components/Menu";
 import GoToTopButton from "@/components/GoToTopButton";
+import Script from "next/script"; // ✅ Import Script
 
 const roboto = Roboto({
   variable: "--font-roboto-sans",
@@ -18,6 +19,26 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google site verification */}
+        <meta
+          name="google-site-verification"
+          content="3IRwYPpAbdgsZkHf91RbsCBqImSHypc8xWXBayOvdcc"
+        />
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DY1EBZHY2N"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DY1EBZHY2N');
+          `}
+        </Script>
+      </head>
       <body className={`${roboto.variable} `}>
         <Menu />
         {children}
